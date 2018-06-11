328da1868076d8173065026b2dc27daf8cbfd6d1
edvitor13 <edvitor13@hotmail.com>
2018-06-11 19:01:02 -0300

    Create kvlang.py

---
+++ Level 01/kvlang.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.lang.builder import Builder
+
+kvcode = Builder.load_string('''
+BoxLayout:
+	orientation: 'vertical'
+	Label:
+		id: saida
+		text: 'Aperte algum botão'
+		font_size: '25dp'
+	Button:
+		text: 'Fritar'
+		font_size: '20dp'
+		on_release:
+			# Acessando Label através do ID e modificando seu texto
+			root.ids.saida.text = 'Batata Frita'
+	Button:
+		text: 'Assar'
+		font_size: '20dp'
+		on_release:
+			# Acessando Label através do ID e modificando seu texto
+			root.ids.saida.text = 'Batata Assada'
+''')
+
+class Programa(App):
+	def build(self):
+		# Retornando o código KvLang
+		return kvcode
+
+Programa().run()
